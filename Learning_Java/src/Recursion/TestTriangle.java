d327c91d7f30a1c58a37f46081b915a341d4b983
PlayerForever <chyneya@gmail.com>
2018-12-31 16:39:40 +0100

    Add "Triangle.java" and TestTriangle

---
+++ Learning_Java/src/Recursion/TestTriangle.java
@@ -0,0 +1,10 @@
+package Recursion;
+
+public class TestTriangle {
+	public static void main(String[] args) {
+		System.out.println(Triangle.getNumber(2));
+		System.out.println(Triangle.getNumberByRecursion(2));
+	}
+	
+	
+}

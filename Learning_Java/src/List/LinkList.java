de1e5266a75a011c5e3b72f5c01b786e372a3076
PlayerForever <chyneya@gmail.com>
2018-12-31 14:18:28 +0100

    Add find() method, delete() method to LinkList.java

--- Learning_Java/src/List/LinkList.java
+++ Learning_Java/src/List/LinkList.java
@@ -9,29 +9,63 @@ public class LinkList {
 	
 	public void insertFirst(long value) { // insert item after head node
 		Node node = new Node(value);
+		node.next = first;
+		first = node;
 //		if(first == null) {
 //			first = node;
 //		} else {
-//			node.next = first.next;
+//			node.next = first;
 //			first = node;
 //		}
-		node.next = first;
-		first = node;
 	}
 	
 	
-	public Node deleteFirst() {  // delete a node after head node
-		Node tmp = first.next;
-		first.next = tmp.next;
+	public Node deleteFirst() {  // delete the first node
+		Node tmp = first;
+		first = tmp.next;
 		return tmp;
 	}
 	
 	public void display() {
 		Node current = first;
-		while(current.next != null) {
+		while(current != null) {
 			current.display();
 			current = current.next;
 		}
+	}
+	
+
+	public Node find(long value) {
+		Node current = first;
+		while(current.data != value) {
+			if(current.next == null) {
+				return null;
+			}
+			current = current.next;
+		}
+		
+		return current;
+	}
+	
+
+	public Node delete(long value) { // delete elements with particular value
+		Node current = first;
+		Node previous = first;
+		while(current.data != value) {
+			if(current.next == null) {
+				return null;
+			}
+			previous = current;
+			current = current.next;
+		}
+		
+		if(current == first) {
+			first = first.next;
+		} else {
+			previous.next = current.next;
+		}
+		
+		return current;
 	}
 	
 	
--- Learning_Java/src/List/TestLinkList.java
+++ Learning_Java/src/List/TestLinkList.java
@@ -6,7 +6,28 @@ public class TestLinkList {
 		linkList.insertFirst(34);
 		linkList.insertFirst(23);
 		linkList.insertFirst(12);
+		linkList.insertFirst(0);
+		linkList.insertFirst(-1);
+		
+//		linkList.display();
+//		System.out.println();
+		
+//		linkList.deleteFirst();
+//		linkList.display();
+//		System.out.println();
+//		
+//		Node node = linkList.find(23);
+//		node.display();
+//		System.out.println();
+//		
+//	
+		Node node1 = linkList.delete(0);
+		node1.display();
+		System.out.println();
 		
 		linkList.display();
+		
+		
+		
 	}
 }
